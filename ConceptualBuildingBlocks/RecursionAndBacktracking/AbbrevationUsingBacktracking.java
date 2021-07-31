96b217f1999bf602a5822655c1d7a22b1a8f5ff2
Himanshu Malik <jsm.28415@gmail.com>
2021-07-31 20:37:48 +0530

    1stQuestion

---
+++ ConceptualBuildingBlocks/RecursionAndBacktracking/AbbrevationUsingBacktracking.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class AbbrevationUsingBacktracking {
+
+    public static void solution(String str, String asf,int count, int pos){
+        //write your code here
+        
+    }
+	public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        solution(str,"",0,0);
+    }
+    
+    
+}
